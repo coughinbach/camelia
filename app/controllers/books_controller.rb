d4ffb930ee6bee6138a9b6837ecdeb8e5e6b5d70
Lola Barbier <lolabarbier@hotmail.com>
2018-01-08 11:00:22 +0100

    need to add owner column to book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,38 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = policy_scope(Book)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    authorize @book
+  end
+
+  def new
+    @book = Book.new
+    authorize @book
+  end
+
+  def create
+    @book = Book.new(book_params)
+    authorize @project
+    @book.owner = current_user
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+
 end
